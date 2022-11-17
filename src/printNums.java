e40a775da80613a312f80f2ea45cd7b02fe2c2de
Ibtehaz Rafid <IbtehazR@nycstudents.net>
2022-11-17 12:27:43 -0500

    Initial commit

---
+++ src/printNums.java
@@ -0,0 +1,17 @@
+public class nums {
+    public nums(){
+
+    }
+
+    public void printNums(int value, int numRounds) {
+        for (int i = 1; i <= numRounds; i++) {
+            int randNum = (int) (Math.random() * 10);
+            while (randNum != value) {
+                System.out.print(randNum);
+                randNum = (int)(Math.random() *10);
+            }
+            System.out.println(value);
+            System.out.println();
+        }
+    }
+}
---
+++ src/printNumsRunner.java
@@ -0,0 +1,6 @@
+public class NumsRunner {
+    public static void main(String[] args) {
+        printNums num = new printNums();
+        num.printNums(3,5);
+    }
+}
